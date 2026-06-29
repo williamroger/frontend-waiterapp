9b01a4db7af159ea72a1a81a12c4749a4c1ddffa
William Roger <williamroger.frontend@gmail.com>
2026-06-29 18:57:23 -0300

    first commit

---
+++ eslint.config.js
@@ -0,0 +1,7 @@
+import json from "@eslint/json";
+import { defineConfig } from "eslint/config";
+
+export default defineConfig([
+  { ignores: ["**/*.js", "**/*.cjs", "**/*.mjs"] },
+  { files: ["**/*.json"], plugins: { json }, language: "json/json", extends: ["json/recommended"] },
+]);
